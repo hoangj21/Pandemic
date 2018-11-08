70f03e752084a6b0633eb70841a3a7bc97922d97
Sarah Schibel <schibel21@up.edu>
2018-11-07 18:59:36 -0800

    Added cards to drawable folder

--- app/src/main/java/SurfaceView.java
+++ app/src/main/java/SurfaceView.java
@@ -11,7 +11,7 @@ import android.view.SurfaceView;
 
 import com.example.joann.pandemic.R;
 
-public class MapView extends SurfaceView {
+class MapView extends SurfaceView {
 
     public MapView(Context context, AttributeSet set) {
         super(context, set);
@@ -28,38 +28,9 @@ public class MapView extends SurfaceView {
         canvas.drawBitmap(map, 100, 100, myPaint );
 
 
-        Bitmap infect3 = BitmapFactory.decodeResource(getResources(), R.drawable.INFECTION3);
+        Bitmap epidemic_card = BitmapFactory.decodeResource(getResources(), R.drawable.PANDEMICCARDS46);
         map = Bitmap.createScaledBitmap(map, 800, 800, true);
         canvas.drawBitmap(map, 100, 100, myPaint );
-
-        Bitmap country11 = BitmapFactory.decodeResource(getResources(), R.drawable.PANDEMICCARDS11);
-        map = Bitmap.createScaledBitmap(map, 800, 800, true);
-        canvas.drawBitmap(map, 100, 100, myPaint );
-
-        Bitmap country13 = BitmapFactory.decodeResource(getResources(), R.drawable.PANDEMICCARDS13);
-        map = Bitmap.createScaledBitmap(map, 800, 800, true);
-        canvas.drawBitmap(map, 100, 100, myPaint );
-
-        Bitmap country16 = BitmapFactory.decodeResource(getResources(), R.drawable.PANDEMICCARDS16);
-        map = Bitmap.createScaledBitmap(map, 800, 800, true);
-        canvas.drawBitmap(map, 100, 100, myPaint );
-
-        Bitmap country29 = BitmapFactory.decodeResource(getResources(), R.drawable.PANDEMICCARDS29);
-        map = Bitmap.createScaledBitmap(map, 800, 800, true);
-        canvas.drawBitmap(map, 100, 100, myPaint );
-
-        Bitmap country30 = BitmapFactory.decodeResource(getResources(), R.drawable.PANDEMICCARDS30);
-        map = Bitmap.createScaledBitmap(map, 800, 800, true);
-        canvas.drawBitmap(map, 100, 100, myPaint );
-
-        Bitmap role3 = BitmapFactory.decodeResource(getResources(), R.drawable.ROLE3);
-        map = Bitmap.createScaledBitmap(map, 800, 800, true);
-        canvas.drawBitmap(map, 100, 100, myPaint );
-
-        Bitmap role5 = BitmapFactory.decodeResource(getResources(), R.drawable.ROLE5);
-        map = Bitmap.createScaledBitmap(map, 800, 800, true);
-        canvas.drawBitmap(map, 100, 100, myPaint );
-
 
     }
 }
